c374d35096f6c387d3c00bb84d07093cd20f0143
allejo <allejo@me.com>
2013-12-22 02:04:33 -0800

    Cleaned up plug-in source code, relicensed to GPLv3, updated README

---
+++ README.markdown
@@ -0,0 +1,16 @@
+# Flag Reset On Cap
+
+A BZFlag plugin that will reset all the unused flags whenever a flag is captured.
+
+## Author
+
+Vladimir "allejo" Jimenez
+
+## Requirements
+* [bzToolkit](https://github.com/allejo/bztoolkit/)
+
+## Compiling
+Be sure the bzToolkit is located in the same folder as your plug-in is when you compile it or you modify the `#include` declaration in the plug-in code respective to the location of the bzToolkit.
+
+## License
+GPL v3
--- flagResetOnCap.cpp
+++ flagResetOnCap.cpp
@@ -1,82 +1,51 @@
 /*
-Copyright (c) 2012 Vladimir Jimenez
-All rights reserved.
+Flag Reset On Cap
+    Copyright (C) 2013 Vladimir Jimenez
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. The name of the author may not be used to endorse or promote products
-   derived from this software without specific prior written permission.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-
-*** Flag Reset On Cap Details ***
-Author:
-Vladimir Jimenez (allejo)
-
-Description:
-Plugin will reset all unused flags except for team flags after each capture event.
-
-Slash Commands:
-N/A
-
-License:
-BSD
-
-Version:
-2.0
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "bzfsAPI.h"
-#include <string>
 
-#define TOTAL_CTF_TEAMS 4
-
-// event handler callback
-class ONCAPHandler : public bz_Plugin
-  {
-  public:
-	virtual const char* Name (){return "Flag Reset On Cap";}
-	virtual void Init ( const char* config);
-    virtual void Event ( bz_EventData *eventData );
-  };
-
-BZ_PLUGIN(ONCAPHandler);
-
-void ONCAPHandler::Init(const char* commandLine)
+class flagResetOnCap : public bz_Plugin
 {
-  bz_debugMessage(4,"flagResetOnCap plugin loaded");
-  Register(bz_eCaptureEvent);
+public:
+    virtual const char* Name (){return "Flag Reset On Cap";}
+    virtual void Init(const char* config);
+    virtual void Event(bz_EventData *eventData);
+};
+
+BZ_PLUGIN(flagResetOnCap)
+
+void flagResetOnCap::Init(const char* commandLine)
+{
+    bz_debugMessage(4, "flagResetOnCap plugin loaded");
+
+    Register(bz_eCaptureEvent);
 }
 
-unsigned int getNumTeams()
+void lastTankStanding::Event(bz_EventData *eventData)
 {
-  return TOTAL_CTF_TEAMS - (!bz_getTeamPlayerLimit(eRedTeam) +
-                            !bz_getTeamPlayerLimit(eGreenTeam) +
-                            !bz_getTeamPlayerLimit(eBlueTeam) +
-                            !bz_getTeamPlayerLimit(ePurpleTeam));
-}
+    switch (eventData->eventType)
+    {
+        case bz_eCaptureEvent:
+        {
+            bz_resetFlags(true, true);
+        }
+        break;
 
-void ONCAPHandler::Event(bz_EventData *eventData)
-{
-  for(int i = getNumTeams(); i < bz_getNumFlags(); i++){
-  	if(bz_flagPlayer(i)==-1)
-      bz_resetFlag(i);
-  }
+        default:
+        break;
+    }
 }
-//Why are you reading this? The source is finished.
